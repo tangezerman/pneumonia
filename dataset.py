20afb0d64e55db941fb26c017582748fea892988
tangezerman <tangezerman@gmail.com>
2025-06-13 22:01:29 +0300

    implement getitem, init, len

--- dataset.py
+++ dataset.py
@@ -5,26 +5,32 @@ from typing import Tuple
 from PIL import Image
 import polars as pl
 
+
 class PneumoniaDataset(Dataset):
-    def __init__(self,csv1:str,csv2:str):
-        self.testdf = pl.read_csv("test.csv")
-        self.traindf = pl.read_csv("train.csv")
+    def __init__(self,csv: str,mode: str):
+        self.df = pl.read_csv(csv)
+        self.testdf = self.df.select("test")
+        self.traindf = self.df.select("train")
         self.mode = mode
         if mode == "train":
-            pass
+            self.df = self.traindf
+            self.column = "train"
         elif mode  == "test":
-            pass
+            self.df = self.testdf
+            self.column = "test"
         else:
             print("mode not set")
 
 
     
 
-    def __getitem__(self, index: int) -> Tuple[torch.Tensor, str]:
-        path = pass
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, str, str]:
+        path = self.df[index,self.column ]
         image = Image.open(path)
         tensor = transforms.ToTensor()(image)
         label = "PNEUMONIA" if "PNEUMONIA" in path else "NORMAL"
 
         return tensor, label, path
     
+    def __len__(self) -> int:
+        return len(self.df)
